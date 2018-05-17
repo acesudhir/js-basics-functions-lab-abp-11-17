f2590b291e7cb423451f8a347798211baf595f69
Sudhir Menon <sudhirhpp@gmail.com>
2018-05-17 17:56:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 // Code your solution in this file!
 
 function distanceFromHqInBlocks (intNum) {
-         if intNum < 42 {
+         if (intNum-42 <0) {
            return 8;
          } else {
            return (intNum - 42);
          }
-
 }
